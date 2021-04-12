d4f235814448acd416cf1126f35710cfcdb75e9c
Carr Chang <carr.z.chang@outlook.com>
2021-04-12 09:08:56 +0800

    Move sources into src directory

--- webpack.config.js
+++ webpack.config.js
@@ -2,27 +2,58 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const FixStyleOnlyEntriesPlugin = require('webpack-fix-style-only-entries')
 
 module.exports = {
+  output: {
+    clean: true
+  },
   module: {
-    rules: [{
-      test: /\.sass$/,
-      use: [
-        {
-          loader: MiniCssExtractPlugin.loader,
-          options: {
-            esModule: false
+    rules: [
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10000,
+              esModule: false,
+              name: 'fonts/[name].[contenthash:7].[ext]'
+            }
           }
-        },
-        {
-          loader: 'css-loader',
-          options: {
-            esModule: false
+        ]
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10000,
+              esModule: false,
+              name: 'fonts/[name].[contenthash:7].[ext]'
+            }
           }
-        },
-        {
-          loader: 'sass-loader'
-        }
-      ]
-    }]
+        ]
+      },
+      {
+        test: /\.sass$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              esModule: false
+            }
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              esModule: false
+            }
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      }
+    ]
   },
   plugins: [
     new FixStyleOnlyEntriesPlugin(),
